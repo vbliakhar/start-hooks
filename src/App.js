bd2a7d2bfad1967a77bfb6ed916bd50cc77da522
vbliakhar <vovabliahar@gmail.com>
2021-01-24 20:56:45 +0100

    useState-start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  function increment() {
+    setCounter(counter + 1);
+  }
+  function decrement() {
+    setCounter(counter - 1);
+  }
+  const test = () => {
+    return setCounter(counter + 6);
+  };
   return (
     <div>
-      <h1>Start my project learns</h1>
+      <h1>
+        <span className="badge badge-warning">Counter:</span>
+        {counter}
+      </h1>
+      <button onClick={increment} className="btn btn-success">
+        add
+      </button>
+      <button onClick={decrement} className="btn btn-danger">
+        subtract
+      </button>
+      <button onClick={test} className="btn btn-dark">
+        Test
+      </button>
     </div>
   );
 }
